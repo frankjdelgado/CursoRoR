1453d4784cf5c869254c28040cc631f29057f926
Frank <frankjdelgado@gmail.com>
2014-06-07 12:09:17 -0430

    editar crear usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,42 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+
+		if User.create(permit_params)
+			flash[:notice] = "New user created succesfully!"
+			redirect_to users_path
+		else
+			render 'new'
+		end
+
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+
+		@user = User.find(params[:id])
+
+		if @user.update(permit_params)
+			flash[:notice] = "User updated succesfully!"
+			redirect_to users_path
+		else
+			render "edit"
+		end
+
+	end
+
+	private
+
+	def permit_params
+		params.require(:user).permit!
+	end
+
 end
